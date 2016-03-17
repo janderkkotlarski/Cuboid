59de39c46db80280bdf0b306c26ca789964fb99f
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-03-17 12:42:25 +0100

    new

--- Cuboid_V0-1.cpp
+++ Cuboid_V0-1.cpp
@@ -7,6 +7,54 @@
 
 #include <SFML/Graphics.hpp>
 
+class cuboid
+{
+	
+	sf::Color m_color{127, 127, 127};
+	
+	sf::Vector3f m_central_posit{0.0f, 0.0f, 0.0f};
+	
+	const float m_side_length{10.0f};
+	
+	std::vector <std::vector <std::vector <sf::Vector3f>>> m_abs_posits;
+
+	void setup_posits()
+	{		
+		m_abs_posits.resize(2);
+		
+		for (int count_1{0}; count_1 < 2; ++count_1)
+		{			
+			m_abs_posits[count_1].resize(2);
+			
+			for (int count_2{0}; count_2 < 2; ++ count_2)
+			{
+				m_abs_posits[count_1][count_2].resize(2);
+			}			
+		}		
+	}
+	
+	void place_posits()
+	{
+		assert(m_side_length > 0.0f);
+		
+		for (int count_1{0}; count_1 < 2; ++count_1)
+		{
+			for (int count_2{0}; count_2 < 2; ++count_2)
+			{
+				for (int count_3{0}; count_3 < 2; ++count_3)
+				{
+					m_abs_posits[count_1][count_2][count_3] =
+					m_central_posit +
+					0.5f*m_side_length*sf::Vector3f((2.0f*count_1 - 1.0f), (2.0f*count_2 - 1.0f), (2.0f*count_3 - 1.0f));
+				}				
+			}		
+		}		
+	}
+	
+	
+	
+};
+
 int main()
 {
 	const std::string program_name{"Cuboid V0.1"};
@@ -28,7 +76,7 @@ int main()
 	
 	const sf::Vector2f window_sizes{window_x, window_y};
 	
-	const sf::Color black{sf::Color(0, 0, 0)};
+	const sf::Color black{0, 0, 0};
 	
 	sf::RenderWindow window{sf::VideoMode(window_x, window_y), program_name, sf::Style::Default};
 	
